b8de1c13cdf3cd30eab83e53dad13c41327856c7
Hajime_Kurita <h_kurita@rise-force.co.jp>
2020-11-16 17:45:17 +0900

    ローカルで見れないエラー

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,40 @@
 <template>
-  <div>
-    <h2>Index page</h2>
-    <ul>
-      <li>
-        <nuxt-link to="/login">ログインページへ</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/authed-route">認証が必要なページへ</nuxt-link>
-      </li>
-    </ul>
-  </div>
+  <section class="container">
+    <div>
+      <h3>Nuxt.js のタグが付けられた投稿の一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4>
+            <span>{{item.title}} </span>
+            <small>
+              <span>by </span>
+              <nuxt-link :to="`/users/${item.user.id}`">
+                {{item.user.id}}
+              </nuxt-link>
+            </small>
+          </h4>
+          <div>{{item.body.slice(0, 130)}}……</div>
+          <p><a target="_blank" :href="item.url">{{item.url}}</a></p>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
-export default{
-  async asyncData({store}){
-    if(store.getters['items'].length){
-    return
+import { mapGetters } from 'vuex';
+
+export default {
+  async asyncData({ store }) {
+    if (store.getters['items'].length) {
+      return;
     }
-    await store.dispatch('fetchItems')
+    await store.dispatch('fetchItems');
   },
-  computed:{
+  computed: {
     ...mapGetters(['items'])
   }
-}
+};
 </script>
 
 <style>
@@ -33,18 +43,19 @@ export default{
   padding: 16px;
 }
 
-h3{
+h3 {
   margin: 16px 0;
-  padding: 8px　0;
+  padding: 8px 0;
   border-bottom: solid 1px #e5e5e5;
 }
 
-li+li{
+li + li {
   margin: 16px 0;
 }
 
-p{
+p {
   margin: 8px 0;
 }
 </style>
+
 
